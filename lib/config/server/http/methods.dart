ee74532232d25b216701ddef4bab200353f4b338
Turing0x <83433135+Turing0x@users.noreply.github.com>
2024-05-07 01:48:26 -0400

    fixed localstorage issue

--- lib/config/server/http/methods.dart
+++ lib/config/server/http/methods.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:frontend_loreal/config/server/http/local_storage.dart';
 import 'package:frontend_loreal/config/utils/glogal_map.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<String> rutaInicial() async {
-  final role = await LocalStorage.getRole();
-  final lastTime = await LocalStorage.getTimeSign();
 
-  if (role != null &&
-      !excede100Minutos(
-          DateTime.parse(lastTime ?? '2020-01-01T00:00:00.000+00:00'))) {
-    return _rutaInicial(role);
+  final prefs = await SharedPreferences.getInstance();
+
+  if (prefs.getBool('first_run') ?? true) {
+    
+    const FlutterSecureStorage storage = FlutterSecureStorage();
+    await storage.deleteAll();
+    prefs.setBool('first_run', false);
+
+    return 'signIn_page';
+  
+  } else {
+    final role = await LocalStorage.getRole();
+    final lastTime = await LocalStorage.getTimeSign();
+
+    if (role != null &&
+        !excede100Minutos(
+            DateTime.parse(lastTime ?? '2020-01-01T00:00:00.000+00:00'))) {
+      return _rutaInicial(role);
+    }
+
+    return 'signIn_page';
   }
 
-  return 'signIn_page';
 }
 
 cerrarSesion(BuildContext context) async {
